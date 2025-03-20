2af98e2341fa7a8b8fd759bbc642ec72b089ca54
GuptaShubham-11 <shu919847@gmail.com>
2025-03-20 18:46:13 +0530

    Refactor component imports and formatting for consistency across Spinner, Toaster, Label, Input, and Button components

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { CircleSlash2 } from "lucide-react";
+import { CircleSlash2 } from 'lucide-react';
 
 export default function Spinner() {
-    return (
-        <div className="flex justify-center items-center h-16">
-            <CircleSlash2 className="w-8 h-8 text-white animate-spin" />
-        </div>
-    );
+  return (
+    <div className="flex justify-center items-center h-16">
+      <CircleSlash2 className="w-8 h-8 text-white animate-spin" />
+    </div>
+  );
 }
