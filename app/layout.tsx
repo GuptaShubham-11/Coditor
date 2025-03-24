5521f9f87b4f2e61b94236f0ddd315a3228fe5e5
GuptaShubham-11 <shu919847@gmail.com>
2025-03-24 12:43:06 +0530

    Integrate ThemeProvider and Header component into RootLayout for enhanced theming and structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,11 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import AuthProvider from '@/context/AuthProvider';
 import { Toaster } from '@/components/ui/sonner';
 import './globals.css';
-import { cn } from '@/lib/utils'; // Ensure utility for handling classNames
+import { cn } from '@/lib/utils';
+import Header from '@/components/Header';
+import { ThemeProvider } from 'next-themes';
 
-// ✅ Load fonts with optimized weights & subsets
+// ✅ Load fonts with optimized settings
 const geistSans = Geist({
   weight: ['400', '500', '600', '700'],
   subsets: ['latin'],
@@ -20,7 +22,7 @@ const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
 });
 
-// ✅ Metadata for SEO & better structure
+// ✅ Metadata for SEO
 export const metadata: Metadata = {
   title: 'Coditor - Online Code Editor',
   description: 'Coditor is a real-time collaborative online code editor.',
@@ -32,17 +34,20 @@ export const metadata: Metadata = {
 // ✅ Root Layout Component
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          'h-full bg-background text-text antialiased',
+          'h-full min-h-screen bg-backgroundL dark:bg-backgroundD text-textL dark:text-textD antialiased selection:bg-accent/90',
           geistSans.variable,
           geistMono.variable,
         )}
       >
         <AuthProvider>
-          {children}
-          <Toaster />
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem >
+            <Toaster />
+            <Header />
+            <main className="h-full min-h-[calc(100vh-4rem)]">{children}</main>
+          </ThemeProvider>
         </AuthProvider>
       </body>
     </html>
